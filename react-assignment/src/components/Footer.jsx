35d089f1e1f2b5ed070eb37abbe68f9749250b56
Hamza Mohtadi <hamzamohtadi225@gmail.com>
2024-03-16 19:26:47 +0600

    updated

---
+++ react-assignment/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+const Footer =()=>{
+    return(
+        <div className="footer">
+            <p>shopping app 2024&copy;</p>
+        </div>
+    )
+}
+export default Footer;
---
+++ react-assignment/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+const Navbar =()=>{
+    return(
+        <div className="navbar">
+            <h2>shopping app</h2>
+        </div>
+    )
+}
+export default Navbar
